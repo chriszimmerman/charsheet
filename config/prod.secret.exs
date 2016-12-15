58b57d5da9faacd9b20855f032b72c2311e1f85e
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-12-15 14:02:16 -0500

    Adds database

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -16,5 +16,5 @@ config :charsheet, Charsheet.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("DATABASE_USERNAME"),
   password: System.get_env("DATABASE_PASSWORD"),
-  database: "charsheet_prod",
+  database: "da4ht6fd9q0lbj",
   pool_size: 20
