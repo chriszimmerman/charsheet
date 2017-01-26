e40eb496701a127df4a85ab4cd1f9b6609c7f27e
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2017-01-25 21:20:45 -0500

    Deleting tests. I currently don't know how to set up a user session for these tests, so I don't know how valuable they are.

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Charsheet.PageControllerTest do
-  use Charsheet.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Characters"
-  end
-end
